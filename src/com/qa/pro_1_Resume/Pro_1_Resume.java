86949de2292b91aa1a4448121a808e988e25fb0a
Aahan14 <mistymugdho@gmail.com>
2022-04-16 12:09:29 -0500

    Project_1-Resume

---
+++ src/com/qa/pro_1_Resume/Pro_1_Resume.java
@@ -0,0 +1,98 @@
+package com.qa.pro_1_Resume;
+
+public class Pro_1_Resume {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	
+	String Name = "Mahfuza Ahmed";
+
+	// Contact Information
+	
+	
+    String Email = "mistymugdho@gmail.com";
+    long Phonenum = 701885905;
+
+    String Address = "715N 40th St. 205H";
+    
+    
+    // Education
+    
+    String Education = "Degree/n, Year, School Name-Location " + 
+    "Degree/n, Year, School Name-Location";
+    
+    // Skills
+    
+    
+    String Skills = "Lorem ipsum dolor sit amet" + 
+    " consectetur adipiscing elit" +" ";
+    
+    
+    //  Professional Summary
+    
+    
+    //String PSummary =" Professional Summary";
+    
+    String PSummary = "Lorem ipsum dolor sit amet,consectetur"
+    		+ " adipiscing elit";
+    	
+	
+    //Experience
+    
+	String EXP ="EXPERIENCE";
+	String EXP1 = "Designation"+"Company Name"+
+	"Month 20xx - Present/n"+"consectetur adipiscing elit"  + "  ";
+	
+    String EXP2 = "Designation"+"Company Name"+ 
+    "Month 20xx - Month 20xx"
+    +"consectetur adipiscing elit"  + "  ";
+    
+    
+    String EXP3 = "Designation"+"Company Name"+ 
+    	    "Month 20xx - Month 20xx"
+    	    +"consectetur adipiscing elit"  + "  ";
+    
+    
+    System.out.println(Name);
+    System.out.println(Email);
+    System.out.println(Phonenum);
+    System.out.println(Address);
+    System.out.println(Education);
+    System.out.println(Skills);
+    System.out.println(PSummary);
+    System.out.println(EXP);
+    System.out.println(EXP1);
+    System.out.println(EXP2);
+    System.out.println(EXP3);
+    System.out.println(Name);
+    System.out.println(Name);
+    System.out.println(Name);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+			
+			
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
